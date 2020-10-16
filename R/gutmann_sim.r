e473dd3599e7353954e4ae0556d1303a46a91614
mkomod <mkomodromos01@gmail.com>
2020-10-16 19:51:34 +0100

    minor changes

--- R/gutmann_sim.r
+++ R/gutmann_sim.r
@@ -1,3 +1,4 @@
+#
 # Replication of Gutmann & Hyvarinen Simulation
 #
 # ------------------------------------------------------------------------------
@@ -7,70 +8,61 @@
 # ------------------------------------------------------------------------------
 #
 library(Rcpp)
-library(mvtnorm)
+library(RcppArmadillo)
 
 Rcpp::sourceCpp("./rlaplace.cpp")
+Rcpp::sourceCpp("./gutman.cpp")
 
 
 # Settings ---------------------------------------------------------------------
+set.seed(123)
 N <- 10^2.4                            # Number of data points
-P <- 4                                 # Dimensionality of data
+P <- 2                                 # Dimensionality of data
 Sig <- 1
+mse <- function(theta, theta.p) mean((theta - theta.p) ^ 2)
 
 
 # Data -------------------------------------------------------------------------
-A <- diag(Sig, P)                        # Covariance Matrix
+A <- matrix(rnorm(P^2, -10, 10), nrow=P, byrow=T)
 X <- t(sapply(1:N, function(tmp) {
     s <- rlaplace(P)
     return( A %*% s)
 }))
-Y <- mvtnorm::rmvnorm(N, mean = rep(0, P), sigma=A)
+Y <- mvtnorm::rmvnorm(N, mean = rep(0, P), sigma=diag(Sig, P))
 
 
 # Estimator --------------------------------------------------------------------
-p.n <- function(x) mvtnorm::dmvnorm(x, rep(0, P), A)
+p.n <- function(x) mvtnorm::dmvnorm(x, rep(0, P), diag(Sig, P))
 
 p.m <- function(x, B, cs) {
-    ln.pm.0 <- -sqrt(2) * sum(abs(t(x) %*% t(B)))
+    ln.pm.0 <- -sqrt(2) * sum(abs(B %*% x))
     return(exp(ln.pm.0 + cs))
 }
 
-h <- function(x, B, c) {
-    p <- p.m(x, B, c)
-    q <- p.n(x)
-    return( p / (p + q) )
+h <- function(x, B, cs) {
+    p.m <- p.m(x, B, cs)
+    p.n <- p.n(x)
+    return( p.m / (p.m + p.n) )
 }
 
 J <- function(theta) {
-    if (theta[1] < 0) {
-	T.y <- T.x <- -Inf
-    } else {
-	B <- solve(diag(theta[1], P))
-	cs <- theta[2]
-	T.x <- apply(X, 1, function(x) { log(h(x, B, cs)) })
-	T.y <- apply(Y, 1, function(y) { 1 - log(h(y, B, cs)) })
-    }
+    B <- solve(matrix(theta[1:(P^2)], nrow=P, byrow=T))
+    cs <- theta[P^2 + 1]
+    T.x <- apply(X, 1, function(x) { log(h(x, B, cs)) })
+    T.y <- apply(Y, 1, function(y) { 1 - log(h(y, B, cs)) })
     return(1 / (2*N) * sum(T.x + T.y))
 }
 
 
 # Optimisation ----------------------------------------------------------------
-set.seed(201014)
-theta.init <- c(abs(rnorm(1, sd=3)), rnorm(1, sd=3)) # Initial theta values
-opt <- optim(theta.init, fn=J,
-	     method="CG",                     # Conjugate Gradient
-	     control=list(fnscale=-1,         # fnscale=1 for max (min by def)
-			  reltol=1e-4,
-			  type=3)          
-)
+J(theta.p)
+theta.init <- rnorm(P^2 + 1)
+opt <- optim(theta.init, fn=J, method="CG", # Conjugate Gradient
+    control=list(fnscale=-1, reltol=1e-7))
 
 
 # Results ---------------------------------------------------------------------
-mse <- function(theta, theta.p) mean((theta - theta.p) ^ 2)
-theta.p  <- c(rep(Sig, P), log(abs(det(A))/4))
-opt.scl <- opt$par / opt$par[1]
-theta.hat <- c(rep(opt.scl[1], P), opt.scl[2])
-
-mse(theta.hat, theta.p) 
+theta.p  <- c(t(A), log(abs(det(A))/4))
+theta.hat <- opt$par
 log10(mse(theta.hat, theta.p))
 
