7f68c45b609257d4e008f10a4a74766048f9f310
mkomod <mkomodromos01@gmail.com>
2020-10-16 11:45:16 +0100

    fix: Search space was incorrect

--- R/gutmann_sim.r
+++ R/gutmann_sim.r
@@ -8,7 +8,6 @@
 #
 library(Rcpp)
 library(mvtnorm)
-library(numDeriv)
 
 Rcpp::sourceCpp("./rlaplace.cpp")
 
@@ -16,13 +15,14 @@ Rcpp::sourceCpp("./rlaplace.cpp")
 # Settings ---------------------------------------------------------------------
 N <- 10^2.4                            # Number of data points
 P <- 4                                 # Dimensionality of data
+Sig <- 1
 
 
 # Data -------------------------------------------------------------------------
-A <- diag(1, P)                        # Covariance Matrix
+A <- diag(Sig, P)                        # Covariance Matrix
 X <- t(sapply(1:N, function(tmp) {
     s <- rlaplace(P)
-    A %*% s
+    return( A %*% s)
 }))
 Y <- mvtnorm::rmvnorm(N, mean = rep(0, P), sigma=A)
 
@@ -30,42 +30,47 @@ Y <- mvtnorm::rmvnorm(N, mean = rep(0, P), sigma=A)
 # Estimator --------------------------------------------------------------------
 p.n <- function(x) mvtnorm::dmvnorm(x, rep(0, P), A)
 
-p.m <- function(x, theta) {
-    A <- matrix(theta[1:(P^2)], nrow = P, byrow=TRUE)
-    B <- solve(A)
-    c <- theta[P^2 + 1]
-    ln.pm.0 <- -sum(apply(B, 1, function(b) sqrt(2) * abs(b %*% x)))
-    return(exp(ln.pm.0 + c))
+p.m <- function(x, B, cs) {
+    ln.pm.0 <- -sqrt(2) * sum(abs(t(x) %*% t(B)))
+    return(exp(ln.pm.0 + cs))
 }
 
-h <- function(x, theta) {
-    return(p.m(x, theta) / (p.m(x, theta) + p.n(x)))
+h <- function(x, B, c) {
+    p <- p.m(x, B, c)
+    q <- p.n(x)
+    return( p / (p + q) )
 }
 
 J <- function(theta) {
-    T.x <- apply(X, 1, function(x) { log(h(x, theta)) })
-    T.y <- apply(Y, 1, function(y) { 1 -log(h(y, theta)) })
+    if (theta[1] < 0) {
+	T.y <- T.x <- -Inf
+    } else {
+	B <- solve(diag(theta[1], P))
+	cs <- theta[2]
+	T.x <- apply(X, 1, function(x) { log(h(x, B, cs)) })
+	T.y <- apply(Y, 1, function(y) { 1 - log(h(y, B, cs)) })
+    }
     return(1 / (2*N) * sum(T.x + T.y))
-}
-
-J.Delta <- function(theta) {           # Gradient of J
-    return(numDeriv::grad(J, theta))
 }
 
 
 # Optimisation ----------------------------------------------------------------
 set.seed(201014)
-theta.init = c(runif(P^2), 0)          # Initial theta values
-opt <- optim(theta.init, fn=J, gr=J.Delta, 
-     method="CG",                      # Conjugate Gradient
-     control=list(fnscale=-1)          # fnscale=1 for max (min by def)
+theta.init <- c(abs(rnorm(1, sd=3)), rnorm(1, sd=3)) # Initial theta values
+opt <- optim(theta.init, fn=J,
+	     method="CG",                     # Conjugate Gradient
+	     control=list(fnscale=-1,         # fnscale=1 for max (min by def)
+			  reltol=1e-4,
+			  type=3)          
 )
 
 
 # Results ---------------------------------------------------------------------
 mse <- function(theta, theta.p) mean((theta - theta.p) ^ 2)
-theta.p  <- c(as.vector(A), log(abs(det(solve(A))) / 4))
-theta.hat <- opt$par
+theta.p  <- c(rep(Sig, P), log(abs(det(A))/4))
+opt.scl <- opt$par / opt$par[1]
+theta.hat <- c(rep(opt.scl[1], P), opt.scl[2])
 
+mse(theta.hat, theta.p) 
 log10(mse(theta.hat, theta.p))
 
