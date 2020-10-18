cef78851784a460c2228ec211560a0ba96949f61
mkomod <mkomodromos01@gmail.com>
2020-10-18 10:29:01 +0100

    fix: singular matrix

--- R/gutmann.r
+++ R/gutmann.r
@@ -4,16 +4,15 @@ library(parallel)
 Rcpp::sourceCpp("rlaplace.cpp")
 Rcpp::sourceCpp("gutmann.cpp")
 
-CORES <- 2
-NUM_DATASETS <- 2
-NUM_START_VALS <- 2
+CORES <- 6
+NUM_DATASETS <- 200
+NUM_START_VALS <- 5
 
 set.seed(2020)
 
 
 generate_data <- function(N) {
-    A <- matrix(round(c(rnorm(4, 5, 1), rnorm(4, 3, 1),
-		  rnorm(4, 4, 1), rnorm(4, 7, 1))), nrow=4)
+    A <- matrix(round(runif(16, 0, 10), 2), nrow=4)
     X <- matrix(rlaplace(4 * N), nrow=N) %*% A
     Y <- matrix(rnorm(4 *  N), nrow=N)
     return(list(X=X, Y=Y, A=A))
@@ -27,20 +26,26 @@ generate_intial_theta <- function() {
 }
 
 
-vals <- mclapply(seq(2, 4, length.out=CORES), function (samp.exp) {
+vals <- mclapply(seq(2, 3.5, length.out=CORES), function (samp.exp) {
     samp.exp <- round(samp.exp)        # round sample size to int
     res <- matrix(0, nrow=NUM_DATASETS, ncol=NUM_START_VALS)
 
     for (i in 1:NUM_DATASETS) {
 	# Generate the data for this run
-	l <- generate_data(10^samp.exp)
-	X <- l$X; Y <- l$Y; A <- l$A          # Unpack list
+	E <- TRUE
+	while (E == TRUE) {
+	    tryCatch({
+		l <- generate_data(10^samp.exp)
+		X <- l$X; Y <- l$Y; A <- l$A          # Unpack list
 
-	# Needed for normal density
-	S <- A %*% t(A)
-	S_inv <- solve(S);
-	lNC <- log(sqrt((2 * pi)^4 * det(S) ))
-	
+		# Needed for normal density
+		S <- A %*% t(A)
+		S_inv <- solve(S);
+		lNC <- log(sqrt((2 * pi)^4 * det(S) ))
+		E <- FALSE
+	    }, error = function(e) E <- TRUE)
+	}	
+
 	# Theta values used to generate the data and Normalising constant
 	theta.p <- c(t(A), log(abs(det(solve(A)))/4))
 
@@ -58,7 +63,7 @@ vals <- mclapply(seq(2, 4, length.out=CORES), function (samp.exp) {
 		res[i, j] <- mean((theta.p - opt$par)^2)
 		cat(".")
 	    }, error = function(e) {
-		print(e)
+		# print(e)
 		res[i, j] <- NA	                     # Sets value to 0 not NA
 		cat("x")
 	    })
